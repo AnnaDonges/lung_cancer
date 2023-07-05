c0dfe2899f03841f750b6b62ca8763f7f4d086d6
AnnaDonges <137080868+AnnaDonges@users.noreply.github.com>
2023-07-05 12:56:24 -0400

    Add files via upload

---
+++ lung_cancer.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
+
+# Load the dataset
+data = pd.read_csv('lung_cancer.csv')
+
+# Separate features (X) and target variable (y)
+X = data.iloc[:, :-1]  # First 15 columns
+y = data.iloc[:, -1]   # Last column
+
+# Encode categorical variables
+label_encoder = LabelEncoder()
+X_encoded = X.apply(label_encoder.fit_transform)
+
+# Perform one-hot encoding
+X_encoded = pd.get_dummies(X_encoded, drop_first=True)
+
+# Encode target variable
+y_encoded = label_encoder.fit_transform(y)
+
+# Split the data into training and testing sets with stratification
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y_encoded, test_size=0.2, stratify=y_encoded, random_state=42)
+
+# Convert the data to numpy arrays
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+# Define the model architecture
+model = Sequential()
+model.add(Dense(64, activation='relu', input_dim=X_train.shape[1]))
+model.add(Dropout(0.5))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(16, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(8, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=10, batch_size=64)
+
+# Evaluate the model
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy:", accuracy)
